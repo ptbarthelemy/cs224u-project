2f090545e751cb96e0cb1e1d7a788ca7d6529d5d
Bartho <bartho@bartho.(none)>
2013-03-12 12:59:36 -0700

    Renamed analyze_poem_features.

--- code/plot_feature_scatter.py
+++ code/plot_feature_scatter.py
@@ -27,20 +27,10 @@ if __name__ == "__main__":
     print "Finding metadata info..."
     scores = getPoemScores(metaDirectory)
 
-    print "Plotting %d feature plots..." %len(next(iter(poems.values())))
+    print "Plotting %d feature plots..." % len(next(iter(poems.values())))
     plt.figure(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
     for index, feature in enumerate(next(iter(poems.values())).keys()):
         print index, feature
         plt.subplot(3, 4, 1 + index)
         plotFeatureVsScore(poems, scores, feature)
-    # plt.show()
     plt.savefig("feature_scatter.pdf", format="pdf")
-
-    # plt.figure(1)                # the first figure
-    # plt.subplot(211)             # the first subplot in the first figure
-    # plt.plot([1,2,3])
-    # plt.subplot(212)             # the second subplot in the first figure
-    # plt.plot([4,5,6])
-
-    # plt.title('Easy as 1,2,3')   # subplot 211 title
-    # plt.show()
