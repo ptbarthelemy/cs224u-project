33bf655d09d1c75bfa70144c08c1a3ff85ce56ed
Jean Wu <jeaneis@gmail.com>
2013-03-16 17:41:42 -0700

    normalized nrc intensity scores

--- code/scrapePoemHunter.py
+++ code/scrapePoemHunter.py
@@ -32,7 +32,7 @@ def crawlPoemPages(page):
   f.close()
   table = soup.findAll('a', {'class':'name'})
   assert(len(table) == 50)
-  for j in range(11, 20):
+  for j in range(30, 40):
     t = table[j]
   #for t in table:
     print t['title']
